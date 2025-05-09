6ce1a18545272be8816b8b8c2f4c1f29d7378d9d
b_abishek <itabishek7@gmail.com>
2025-05-09 20:33:09 +0530

    Update AboutSection.tsx

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -6,7 +6,7 @@ import { FileText } from "lucide-react";
 const AboutSection: React.FC = () => {
   const openResume = () => {
     // Replace this URL with the actual URL to your resume PDF
-    const resumeUrl = "/public/ResumeCv.pdf";
+    const src = "/public/ResumeCv.pdf";
     window.open(resumeUrl, "_blank");
   };
   
