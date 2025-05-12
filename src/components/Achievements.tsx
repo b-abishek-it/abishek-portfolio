4c347dafe0e6be3700a2cee67e33351c804bf77b
b_abishek <itabishek7@gmail.com>
2025-05-12 19:46:21 +0530

    Update Achievements.tsx

--- src/components/Achievements.tsx
+++ src/components/Achievements.tsx
@@ -16,10 +16,10 @@ const Achievements = () => {
   
   // Replace with actual achievement data when available
   const achievements = [
-    { id: 1, image: "/public/c1.jpg", title: "Introduction to Microsoft Excel" },
-    { id: 2, image: "/public/C2.jpg", title: "TCS iON Career Edge - Young Professional" },
-    { id: 3, image: "/public/C3.jpg", title: "Workshop" },
-    { id: 4, image: "/public/C4.jpg", title: "Internship" }
+    { id: 1, image: "/C1.jpg", title: "Introduction to Microsoft Excel" },
+    { id: 2, image: "/C2.jpg", title: "TCS iON Career Edge - Young Professional" },
+    { id: 3, image: "/C3.jpg", title: "Workshop" },
+    { id: 4, image: "/C4.jpg", title: "Internship" }
   ];
 
   const nextImage = () => {
